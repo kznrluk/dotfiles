cbb56080b92bae573f2e6ef14a281873047a18df
Kazunari Sasa <kznrluk@anyfrog.net>
2024-04-19 14:53:41 +0900

    update

--- Profile.ps1
+++ Profile.ps1
@@ -18,17 +18,16 @@ Set-Alias -Name ka -Value Select-KubeContext
 
 function Get-GKEClusterCredentials {
     $clusters = gcloud container clusters list --format="value(name, location)" | Out-String
-    $clusterLines = $clusters -split "`n"
+    $clusterLines = $clusters -split "`n" | Where-Object { $_ -ne "" }
     foreach ($line in $clusterLines) {
         $clusterData = $line -split '\s+'
         $clusterName = $clusterData[0]
         $clusterLocation = $clusterData[1]
 
-        $command = "gcloud container clusters get-credentials $clusterName --zone $clusterLocation"
+        $command = "gcloud container clusters get-credentials $clusterName --region $clusterLocation"
         Write-Host "Running command: $command"
         Invoke-Expression $command
     }
 }
 
 $env:RUNEWIDTH_EASTASIAN = 0
-$env:EDITOR = "micro"
